d23f143ae9fbefce59fc19de315cf8c766636d24
k <1025023440@qq.com>
2019-05-09 16:58:07 +0800

    加入支付宝原来的日志类并定义日志常量

--- Kpay.php
+++ Kpay.php
@@ -8,6 +8,25 @@
 
 defined('DS') or define('DS',DIRECTORY_SEPARATOR);
 
+/**
+* SDK工作目录
+* 存放日志，TOP缓存数据
+*/
+if (!defined("TOP_SDK_WORK_DIR"))
+{
+        define("TOP_SDK_WORK_DIR", "/tmp/");
+}
+
+/**
+* 是否处于开发模式
+* 在你自己电脑上开发程序的时候千万不要设为false，以免缓存造成你的代码修改了不生效
+* 部署到生产环境正式运营后，如果性能压力大，可以把此常量设定为false，能提高运行速度（对应的代价就是你下次升级程序时要清一下缓存）
+*/
+if (!defined("TOP_SDK_DEV_MODE"))
+{
+        define("TOP_SDK_DEV_MODE", true);
+}
+
 class Kpay {
     //应用ID,您的APPID。
 	public $appId  = '2016091800536922';
